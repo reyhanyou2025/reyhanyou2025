3fdb1f2ca67c0dbad186f300ef50c502236743a2
reyhanyou2025 <yousefiyan.ry@gmail.com>
2025-04-06 09:53:43 +0330

    Create __manifest__.py

---
+++ my_module/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'My Custom Module',
+    'version': '1.0',
+    'category': 'Custom',
+    'author': 'Your Name',
+    'depends': ['base'],
+    'data': [
+        'views/my_model_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
